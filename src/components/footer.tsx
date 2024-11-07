d6a55613ceb4d0fa86475af2dd12de611940df88
Amangupta0000 <amangupta1a2b3c@gmail.com>
2024-11-07 16:56:29 +0530

    Project completed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <div className="mt-6 p-6 text-gray-500 text-center text-[13px]">
-            Made by @jansm04 in 2024&nbsp; •&nbsp; 
+            Made by Aman Gupta (Roll No : 22bcs012)  & Honey Rajput (Roll No : 22bcs047 )&nbsp; •&nbsp; 
             {<Link href='https://github.com/jansm04/dav' target="_blank">
                 <div className="text-sky-700 inline"> Github </div>
             </Link>}
